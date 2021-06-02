b003f13a732535fb603909a28256d4c4bf21b672
Jorge <jorgeatgu@gmail.com>
2021-06-02 23:37:09 +0200

    update post-process

--- postprocess.js
+++ postprocess.js
@@ -1,24 +1,30 @@
-import { readJSON, writeJSON } from 'https://deno.land/x/flat@0.0.9/mod.ts' 
+import { readJSON, writeJSON } from 'https://deno.land/x/flat@0.0.9/mod.ts'
 
 const filename = 'price.json';
 const json = await readJSON(filename)
 
 const filteredData = json.PVPC.map(({ Dia, Hora, PCB }) => {
+  const getFirstHour = Hora.split('-')[0];
   return {
     day: Dia,
-    hour: Hora.split('-')[0],
-    price: +(PCB.split(',')[0]) / 1000,
-    zone: getZone(hour)
-  }
-})
+    hour: +getFirstHour,
+    price: +PCB.split(',')[0] / 1000,
+    zone: getZone(+getFirstHour)
+  };
+});
 
 function getZone(hour) {
-  if(hour >= "0" && hour < "8") {
-    return 'valle'
-  } else if(hour >= "8" && hour < "10" || hour >= "14" && hour < "18" || hour >= "22" && hour < "24") {
-    return 'llano'
+  console.log('hour', hour);
+  if (hour >= 0 && hour < 8) {
+    return 'valle';
+  } else if (
+    (hour >= 8 && hour < 10) ||
+    (hour >= 14 && hour < 18) ||
+    (hour >= 22 && hour < 24)
+  ) {
+    return 'llano';
   } else {
-    return 'punta'
+    return 'punta';
   }
 }
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,12 @@
 import './../css/styles.css';
-/*import data from '../../price-postprocessed.json';*/
-import dataPrice from '../../price.json';
+import data from '../../price-postprocessed.json';
 
-/*const timeZoneUser = Intl.DateTimeFormat().resolvedOptions().timeZone;
+const timeZoneUser = Intl.DateTimeFormat().resolvedOptions().timeZone;
 const userHour = new Date().getHours();
-const [{ price, zone }] = data.filter(({ hour }) => +hour === userHour);
+const [{ price, zone }] = data.filter(({ hour }) => hour === userHour);
 
 const getPriceElement = document.getElementById('price');
 
 function backgroundZone(zone) {}
 
-getPriceElement.textContent = `${price}€`;*/
-
-const filteredData = dataPrice.PVPC.map(({ Dia, Hora, PCB }) => {
-  const getFirstHour = Hora.split('-')[0];
-  return {
-    day: Dia,
-    hour: getFirstHour,
-    price: +PCB.split(',')[0] / 1000,
-    zone: getZone(+getFirstHour)
-  };
-});
-
-function getZone(hour) {
-  console.log('hour', hour);
-  if (hour >= 0 && hour < 8) {
-    return 'valle';
-  } else if (
-    (hour >= 8 && hour < 10) ||
-    (hour >= 14 && hour < 18) ||
-    (hour >= 22 && hour < 24)
-  ) {
-    return 'llano';
-  } else {
-    return 'punta';
-  }
-}
-
-console.log('filteredData', filteredData);
+getPriceElement.textContent = `${price}€`;
