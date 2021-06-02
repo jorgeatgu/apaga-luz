6ab051b6db0dbc9cdb9fee4ca9c8961368ffd4b8
Jorge <jorgeatgu@gmail.com>
2021-06-02 22:52:10 +0200

    simplify post-process

--- postprocess.js
+++ postprocess.js
@@ -6,22 +6,10 @@ const json = await readJSON(filename)
 const filteredData = json.PVPC.map(({ Dia, Hora, PCB }) => {
   return {
     day: Dia,
-    firstHour: parseDate(Hora),
-    secondHour: parseDate(Hora, true),
+    hour: +(hora.split('-')[0]),
     price: +(PCB.split(',')[0]) / 1000
   }
 })
-
-function parseDate(hora, second = false) {
-  let hour = +(hora.split('-')[0])
-  hour = second ? hour + 1 : hour
-  const todayIs = new Date()
-  const year = todayIs.getFullYear()
-  const month = todayIs.getMonth()
-  const day = todayIs.getDay()
-
-  return new Date(year, month, day, hour, "00")
-}
 
 const newFilename = `price-postprocessed.json`;
 await writeJSON(newFilename, filteredData)
--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,12 @@ import './../css/styles.css';
 import data from '../../price-postprocessed.json';
 
 console.log(data);
-
-const firstDate = new Date();
-const secondDate = new Date();
-console.log('firstDate', firstDate);
-secondDate.setHours(secondDate.getHours() + 1);
-console.log('secondDate', secondDate);
 const timeZoneUser = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+const userHour = new Date().getHours();
+console.log('userHour', userHour);
+
+const getHour = data.filter(({ firstHour }) =>
+  console.log(new Date(firstHour).getHours() - 1)
+);
+console.log('getHour', getHour);
