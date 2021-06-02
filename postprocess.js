0c53cd3e12a28ac74f7e91a148f7a5ff0ba8005e
Jorge <jorgeatgu@gmail.com>
2021-06-02 21:42:47 +0200

    json

--- postprocess.js
+++ postprocess.js
@@ -3,7 +3,7 @@ import { readJSON, writeJSON } from 'https://deno.land/x/flat@0.0.9/mod.ts'
 const filename = 'price.json';
 const json = await readJSON(filename)
 
-const filteredData = data.PVPC.map(({ Dia, Hora, PCB }) => {
+const filteredData = json.PVPC.map(({ Dia, Hora, PCB }) => {
   return {
     dia: Dia,
     hora: parseDate(Hora),
