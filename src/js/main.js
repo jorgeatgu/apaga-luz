88a653249dde01a4d17af395b72c3f26d97bdef0
Jorge <jorgeatgu@gmail.com>
2021-06-02 23:29:46 +0200

    remove files

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,10 @@ import data from '../../price-postprocessed.json';
 
 const timeZoneUser = Intl.DateTimeFormat().resolvedOptions().timeZone;
 const userHour = new Date().getHours();
-const [{ price }] = data.filter(({ hour }) => +hour === userHour);
+const [{ price, zone }] = data.filter(({ hour }) => +hour === userHour);
 
 const getPriceElement = document.getElementById('price');
+
+function backgroundZone(zone) {}
 
 getPriceElement.textContent = `${price}€`;
