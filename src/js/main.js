4b0a023faf1c1b7c4576158e74ea58e532d2203a
Jorge <jorgeatgu@gmail.com>
2021-06-03 00:57:16 +0200

    build

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 import './../css/styles.css';
-import data from '../../public/data/price-postprocessed.json';
+import data from '../../public/price-postprocessed.json';
 
 let userHour = new Date().getHours();
 let userMinutes = new Date().getMinutes();
