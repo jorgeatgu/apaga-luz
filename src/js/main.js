37a824ba9209f18ecffe2d6d23cf4da158e34176
Jorge <jorgeatgu@gmail.com>
2021-10-07 23:38:09 +0200

    create a new table to show the prices for tomorrow

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,18 @@
 import './../css/styles.css';
 import data from '../../public/price-postprocessed.json';
+import dataNextDay from '../../public/price-next-day.json';
 import { week, weekEnd } from './templates.js';
 import {
   nextCheapHour,
   reloadPage,
   tablePrice,
+  tablePriceNextDay,
   getZoneColor
 } from './utils.js';
 
 let userHour = new Date().getHours();
 let userMinutes = new Date().getMinutes();
 let userDay = new Date().getDay();
-console.log('userDay', userDay);
 
 const [{ price, zone }] = data.filter(({ hour }) => +hour === userHour);
 
@@ -61,3 +62,24 @@ reverseCheapHours = reverseCheapHours
 
 tablePrice(reverseCheapHours, 'cheap-element');
 tablePrice(expensiveHours, 'expensive-element');
+
+/*
+Prices are published at 20:30,
+at 21:00 I publish the next day's data,
+this table will only be available until 24:00.
+*/
+
+const containerTableNextDay = document.querySelector('.table-next-day');
+if (userHour >= 21 && userHour <= 24) {
+  containerTableNextDay.style.display = 'grid';
+  const filterDataNextDay = dataNextDay.map(({ price, ...rest }) => {
+    return {
+      price: price.toFixed(3),
+      ...rest
+    };
+  });
+
+  tablePriceNextDay(filterDataNextDay);
+} else {
+  containerTableNextDay.style.display = 'none';
+}
