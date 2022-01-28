7f8270ec847db1c45ff3b4f711e0edb5840b12e2
Jorge <jorgeatgu@gmail.com>
2022-01-28 21:24:57 +0100

    data: update the function to create datasets

--- data/create-datasets.js
+++ data/create-datasets.js
@@ -11,7 +11,7 @@ const filteredData = json.map(({ dia, hora, precio }) => {
   };
 });
 
-const reduced = filteredData.reduce(function(m, d) {
+const reduced = filteredData.reduce((m, d) => {
   if (!m[d.dia]) {
     m[d.dia] = { ...d, count: 1 };
     return m;
@@ -21,15 +21,40 @@ const reduced = filteredData.reduce(function(m, d) {
   return m;
 }, {});
 
-const result = Object.keys(reduced).map(function(k) {
-  const item = reduced[k];
+const groupDataByDay = Object.keys(reduced).map((itemByDay) => {
+  const item = reduced[itemByDay];
   return {
-    dia: item.dia,
-    precio: (item.precio / item.count).toFixed(2),
+    price: +(item.precio / item.count).toFixed(2),
+    day: item.dia.split('/')[0],
+    month: item.dia.split('/')[1],
+    year: item.dia.split('/')[2],
+    date: `${item.dia.split('/')[1]}/${item.dia.split('/')[0]}/${item.dia.split('/')[2]}`,
+    monthYear: `${item.dia.split('/')[1]}/${item.dia.split('/')[2]}`
   }
 })
 
-const resultJSON = result
+const reducedByMonth = groupDataByDay.reduce((m, d) => {
+  if (!m[d.monthYear]) {
+    m[d.monthYear] = { ...d, count: 1 };
+    return m;
+  }
+  m[d.monthYear].price += d.price;
+  m[d.monthYear].count += 1;
+  return m;
+}, {});
 
-const newFilename = 'group-by-day.json';
-await writeJSON(newFilename, resultJSON)
+const groupDataByMonth = Object.keys(reducedByMonth).map((itemByMonth) => {
+  const item = reducedByMonth[itemByMonth];
+  return {
+    averagePrice: +(item.price / item.count).toFixed(2),
+    monthYear: item.monthYear,
+    month: item.month,
+    date: item.date,
+    year: item.year
+  }
+})
+
+const newFileByDay = 'group-by-day.json';
+const newFileByMonth = 'group-by-month.json';
+await writeJSON(newFileByDay, groupDataByDay)
+await writeJSON(newFileByMonth, groupDataByMonth)
