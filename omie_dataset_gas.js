464fe1ad4c54991d44755154ff249951003fdbef
Jorge <jorgeatgu@gmail.com>
2022-08-24 23:39:26 +0200

    compensacion: update github-action to update gas history every day

--- omie_dataset_gas.js
+++ omie_dataset_gas.js
@@ -49,4 +49,8 @@ let omie_compensacion = compensacion_csv_to_json.map((element, index) => {
 });
 omie_compensacion = omie_compensacion.filter(({ precio }) => precio);
 
+const omie_compensacion_historic = await readJSON('public/data/historic_compensacion_gas.json');
+const omie_compensacion_historic_update = [...omie_compensacion, ...omie_compensacion_historic]
+
 await writeJSON('public/data/omie_compensacion_data.json', omie_compensacion)
+await writeJSON('public/data/historic_compensacion_gas.json', omie_compensacion_historic_update)
