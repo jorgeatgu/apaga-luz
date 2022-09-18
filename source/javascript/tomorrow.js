7687df37c399bb1afa63ef1af00242d70d78779b
Jorge <jorgeatgu@gmail.com>
2022-09-18 23:05:42 +0200

    titles

--- source/javascript/tomorrow.js
+++ source/javascript/tomorrow.js
@@ -164,8 +164,7 @@ const text_whatsApp = `whatsapp://send?text=Aquí puedes consultar el precio de 
 )} https://www.apaga-luz.com/precio-luz-manana/?utm_source=whatsapp_mnn`;
 const button_whatsApp = document.getElementById('btn-whatsapp-manana');
 button_whatsApp.href = text_whatsApp;
-
-document.title = `Precio de la luz mañana ${tomorrow.toLocaleDateString(
+document.title = `👉🏻 Precio de la luz mañana ${tomorrow.toLocaleDateString(
   'es-ES',
   options
-)} | Consulta ahora`;
+)} | Consulta ahora ✅`;
