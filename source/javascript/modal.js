d23d94322ccd1bf90b9d94f2b31de23813b14a86
Jorge <jorgeatgu@gmail.com>
2023-08-29 21:34:38 +0200

    refactor js

---
+++ source/javascript/modal.js
@@ -0,0 +1,68 @@
+export function show_modal() {
+  setTimeout(() => {
+    const modalState = JSON.parse(localStorage.getItem('modalState'));
+    if (
+      !modalState ||
+      (modalState.closed && Date.now() > modalState.expiration)
+    ) {
+      let user_hour = new Date().getHours();
+      let user_minutes = new Date().getMinutes();
+      const QUARTER_PAST_ONE = 790;
+      user_hour = user_hour < 10 ? `0${user_hour}` : user_hour;
+      user_minutes = user_minutes < 10 ? `0${user_minutes}` : user_minutes;
+
+      const its_time_to_show_the_content =
+        user_hour * 60 + +user_minutes >= QUARTER_PAST_ONE && user_hour < 24;
+
+      const currentURL = window.location.href;
+      const modalHtml = its_time_to_show_the_content
+        ? `
+      <div class="modal-container">
+      <div class="modal-background"></div>
+        <div id="modal" class="modal-adsense">
+          <center>
+            <h1>Precio de la luz mañana</h1>
+            <p><a href="https://www.apaga-luz.com/precio-luz-manana/" class="button">VER</a></p>
+            <p><a href="?=" class="reload-button">CERRAR</a></p>
+          </center>
+        </div>
+      `
+        : `
+      <div class="modal-container">
+      <div class="modal-background"></div>
+        <div id="modal" class="modal-adsense">
+          <center>
+            <h1>Datos del precio de la luz</h1>
+            <p><a href="https://www.apaga-luz.com/graficas/" class="button">VER</a></p>
+            <p><a href="?=" class="reload-button">CERRAR</a></p>
+          </center>
+        </div>
+      `;
+
+      const modalContainer = document.createElement('div');
+      modalContainer.innerHTML = modalHtml;
+
+      document.body.appendChild(modalContainer);
+
+      const cerrarEnlaces = document.querySelectorAll(
+        '.reload-button, .button'
+      );
+      cerrarEnlaces.forEach(enlace => {
+        enlace.addEventListener('click', () => {
+          const now = new Date();
+          const expirationDate = new Date(now.getTime() + 3600000);
+          const modalState = {
+            closed: true,
+            expiration: expirationDate.getTime()
+          };
+          localStorage.setItem('modalState', JSON.stringify(modalState));
+
+          const modal = document.getElementById('modal');
+          if (modal) {
+            modal.style.display = 'none';
+          }
+        });
+      });
+    }
+  }, 5000);
+}
--- source/javascript/tomorrow.js
+++ source/javascript/tomorrow.js
@@ -4,6 +4,7 @@ import data_tomorrow_omie from '/public/data/omie_data.json';
 import data_gas_omie from '/public/data/omie_compensacion_data.json';
 import { table_price_tomorrow, remove_tables_tomorrow } from './table.js';
 import { is_week_end } from './utils.js';
+import { show_modal } from './modal.js';
 
 /*
 Prices are published at 20:15,
@@ -182,65 +183,4 @@ const text_whatsApp = `whatsapp://send?text=Aquí puedes consultar el precio de 
 const button_whatsApp = document.getElementById('btn-whatsapp-manana');
 button_whatsApp.href = text_whatsApp;
 
-// Función para crear el modal
-function crearModal() {
-  const currentURL = window.location.href; // Obtiene la URL actual
-  const modalHtml = `
-  <div class="modal-container">
-  <div class="modal-background"></div>
-    <div id="modal" class="modal-adsense">
-      <center>
-        <h1>Datos del precio de la luz</h1>
-        <p><a href="https://www.apaga-luz.com/graficas/" class="button">VER</a></p>
-        <p><a href="?=" class="reload-button">CERRAR</a></p>
-      </center>
-    </div>
-  `;
-
-  const modalContainer = document.createElement('div');
-  modalContainer.innerHTML = modalHtml;
-
-  document.body.appendChild(modalContainer);
-
-  // Agregar evento al enlace "Cerrar"
-  const cerrarEnlaces = document.querySelectorAll('.reload-button,.button');
-  cerrarEnlaces.forEach(enlace => {
-    enlace.addEventListener('click', () => {
-      // Al hacer clic en "Cerrar", almacenar la fecha actual junto con el estado en localStorage
-      const now = new Date();
-      const expirationDate = new Date(now.getTime() + 3600000); // Una hora en milisegundos
-      const modalState = {
-        closed: true,
-        expiration: expirationDate.getTime()
-      };
-      localStorage.setItem('modalState', JSON.stringify(modalState));
-
-      // Ocultar el modal
-      ocultarModal();
-    });
-  });
-}
-
-// Función para ocultar el modal
-function ocultarModal() {
-  const modal = document.getElementById('modal');
-  if (modal) {
-    modal.style.display = 'none';
-  }
-}
-
-// Función para mostrar el modal después de 5 segundos, a menos que esté cerrado en localStorage
-function mostrarModalDespuesDe5Segundos() {
-  setTimeout(() => {
-    const modalState = JSON.parse(localStorage.getItem('modalState'));
-    if (
-      !modalState ||
-      (modalState.closed && Date.now() > modalState.expiration)
-    ) {
-      crearModal();
-    }
-  }, 5000); // 5000 milisegundos (5 segundos)
-}
-
-// Llama a la función para mostrar el modal después de 5 segundos
-mostrarModalDespuesDe5Segundos();
+show_modal();
